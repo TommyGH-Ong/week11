7f62c7ea0ec33df618969484c967e9a63c643adf
Tommy <tommy@gmail.com>
2023-03-20 11:04:51 -0700

    Array Practice adding DOM elements with JS

--- scripts/script.js
+++ scripts/script.js
@@ -24,32 +24,53 @@
 // }
 // getvalue()
 
-const enteredValue = prompt("Enter a value")
+// const enteredValue = prompt("Enter a value")
 
-//references to p tag
-const radiusPara = document.querySelector("#radius");
-radiusPara.textContent = radiusPara.textContent + " " + enteredValue
-// console.log(radiusPara)
-const resultPara = document.querySelector("#result");
-// console.log(resultPara)
+// //references to p tag
+// const radiusPara = document.querySelector("#radius");
+// radiusPara.textContent = radiusPara.textContent + " " + enteredValue
+// // console.log(radiusPara)
+// const resultPara = document.querySelector("#result");
+// // console.log(resultPara)
 
-function calculateArea(radius){
-// check if radius is not a number
-    if(isNaN(radius)){
-        // alert("This is not a number");
-        // update the resultPara to show the error
-        resultPara.textContent = "This is not a number"
-    }
-    else {
-        const area = Math.PI * radius * radius;
-        return area.toFixed(2);
-    }
+// function calculateArea(radius){
+// // check if radius is not a number
+//     if(isNaN(radius)){
+//         // alert("This is not a number");
+//         // update the resultPara to show the error
+//         resultPara.textContent = "This is not a number"
+//     }
+//     else {
+//         const area = Math.PI * radius * radius;
+//         return area.toFixed(2);
+//     }
+// }
+// let result = calculateArea(enteredValue)
+// if (result){
+//     //update the radiusPara
+//     // alert(`The area of a circle with radius ${enteredValue} is ${result}`)
+//     resultPara.textContent = `The area of a circle with radius ${enteredValue} is ${result}`
+// }
+
+function populateList(myShoppingList){
+    //get access to the ul element
+    const ulElement = document.querySelector(".shopping")
+    // console.log(ulElement)
+
+    //loop through myShoppingList array
+    for (let item of myShoppingList) {
+        //console.log(item);
+        //make a new li document.createElement
+        const newLi = document.createElement("li");
+        //update the text of the new li
+        newLi.textContent = item;
+        //append the new li under ul
+        ulElement.appendChild(newLi)
+    } 
+    //add an li under the ul element with text equal to each item of myShoppingList
+ 
+
 }
-let result = calculateArea(enteredValue)
-if (result){
-    //update the radiusPara
-    // alert(`The area of a circle with radius ${enteredValue} is ${result}`)
-    resultPara.textContent = `The area of a circle with radius ${enteredValue} is ${result}`
-}
 
-
+let shoppingList = ['bread', 'cheese', 'green pepper'];
+populateList(shoppingList)
