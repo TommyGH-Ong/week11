463dd8a0b615f16ffc22690fa282d6deb1888d68
Tommy <tommy@gmail.com>
2023-03-20 09:28:02 -0700

    Wrote greet function as expression and arrow

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,18 @@
+//function declaration
 function greet(inputname) {
     console.log(`Hello ${inputname}!`);
 };
 
-greet("Tommy")
+//function expression
+const greet = function(inputname)
+{
+    console.log(`Hello ${inputname}!`);
+};
+
+//arrow function
+let greet = inputname => {
+    console.log(`Hello ${inputname}!`);
+};
+
+//function calling
+greet("Tommy");
