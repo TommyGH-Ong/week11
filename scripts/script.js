5ccb0e6ead16eec4487f8ec0507c2760e7fd4ad3
Tommy <tommy@gmail.com>
2023-03-20 09:41:00 -0700

    practice with parameters and default

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 //function declaration
-function greet(inputname) {
-    console.log(`Hello ${inputname}!`);
+function greet(inputname, time="day") {
+    console.log(`Good ${time} ${inputname}!`);
 };
 
-//function expression
-const greet = function(inputname)
-{
-    console.log(`Hello ${inputname}!`);
-};
+// //function expression
+// const greet = function(inputname)
+// {
+//     console.log(`Hello ${inputname}!`);
+// };
 
-//arrow function
-let greet = inputname => {
-    console.log(`Hello ${inputname}!`);
-};
+// //arrow function
+// let greet = inputname => {
+//     console.log(`Hello ${inputname}!`);
+// };
 
 //function calling
 greet("Tommy");
